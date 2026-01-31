b2ab3188e5de6ba61b1be360274db33a16378a76
Sahith-2106 <ksahith456@gmail.com>
2026-01-31 15:01:41 +0100

    Third Change

--- src/app/backend_data/about.data.ts
+++ src/app/backend_data/about.data.ts
@@ -2,9 +2,9 @@ export type Highlight = { icon: string; title: string; desc: string; };
 export type Stat = { value: string; label: string; colorClass?: string; };
 
 export const HIGHLIGHTS: Highlight[] = [
-  { icon: 'language',  title: 'Web Development',    desc: 'Responsive, accessible apps with modern frameworks.' },
-  { icon: 'code-merge',   title: 'DevOps', desc: 'Automation, Deployment and CI/CD' },
-  { icon: 'cloud-binary',  title: 'Cloud Enthusiast', desc: 'Honing skills in Azure and cloud based development ' },
+  { icon: 'language',  title: 'Data Architecture & Modeling',    desc: 'Designing scalable data models and architectures for performance, consistency, and analytics readiness.' },
+  { icon: 'code-merge',   title: 'Cloud Pipelines & Automation', desc: 'Building automated, production-grade data pipelines using cloud-native services and CI/CD.' },
+  { icon: 'cloud-binary',  title: 'Stakeholder-Ready Analytics', desc: 'Delivering trusted metrics and insights that drive confident business decisions.' },
 ];
 
 export const STATS: Stat[] = [
